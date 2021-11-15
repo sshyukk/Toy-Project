d2f5f4c70fee43ab27a83020ec6546f45eeca2c0
sshyukk <sanghyuk0328@gmail.com>
2021-11-15 20:21:02 +0900

    function에서 arrow function으로 변경

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate.js"
 import Card from "../UI/Card.js"
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
