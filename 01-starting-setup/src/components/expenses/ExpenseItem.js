c8a8fbfc4df43d6e384a6fbc70c2f92343edea17
sshyukk <sanghyuk0328@gmail.com>
2021-11-16 15:03:51 +0900

    이벤트 리스닝, 이벤트 핸들러 정의

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate.js"
 import Card from "../UI/Card.js"
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Click!!!')
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
@@ -11,6 +14,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>click</button>
         </Card>
     )
 }
