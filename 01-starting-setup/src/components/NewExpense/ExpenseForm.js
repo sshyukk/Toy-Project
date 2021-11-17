c43a1360e5a0690afd8f668c6936cdf47904ccf1
sshyukk <sanghyuk0328@gmail.com>
2021-11-17 16:05:54 +0900

    여러 state를 하나로 관리

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,21 +2,41 @@ import React, { useState } from "react"
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('')
-    const [enteredAmount, setEnteredAmount] = useState('')
-    const [enteredDate, setEnteredDate] = useState('')
+    // const [enteredTitle, setEnteredTitle] = useState('')
+    // const [enteredAmount, setEnteredAmount] = useState('')
+    // const [enteredDate, setEnteredDate] = useState('')
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
-        console.log(enteredTitle)
+        // setEnteredTitle(event.target.value)
+        // console.log(enteredTitle)
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        })
+        console.log(userInput)
     }
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value)
-        console.log(enteredAmount)
+        // setEnteredAmount(event.target.value)
+        // console.log(enteredAmount)
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        })
+        console.log(userInput)
     }
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value)
-        console.log(enteredDate)
+        // setEnteredDate(event.target.value)
+        // console.log(enteredDate)
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
+        console.log(userInput)
     }
     return (
         <form>
